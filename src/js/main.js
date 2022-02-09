cf4f132a2c2c2192b9bb40a618f485288ef2049e
javier cruz obispo <javier.cruz.odispo@hotmail.com>
2022-02-09 11:35:56 -0600

    basic input data and basic print info

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,16 @@
-console.log("Run!!!!")
+document.addEventListener("DOMContentLoaded",function(){
+    const form  = document.getElementById("post_form");
+    const ItemModel = document.querySelector("template.modelItem");
+    form.querySelector("#post_date").value = new Date().toISOString().slice(0,10); 
+    form.onsubmit = e=>{
+        e.preventDefault();
+        const title = e.target.querySelector("#post_title").value,
+        desc  = e.target.querySelector("#post_description").value,
+        date  = e.target.querySelector("#post_date").value;
+        let clone = ItemModel.content.cloneNode(true);
+        clone.querySelector(".Title").innerText = title;
+        clone.querySelector(".Description").innerText = desc;
+        clone.querySelector(".Date").innerText = date;
+        document.querySelector("#print").appendChild(clone);
+    }
+})
