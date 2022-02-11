d34328bb90a2c61cae8094d06c5405b2f28ad1e6
javier cruz obispo <javier.cruz.odispo@hotmail.com>
2022-02-10 20:35:28 -0600

    update system

--- src/js/main.js
+++ src/js/main.js
@@ -1,64 +1,69 @@
 let api = new TodoApp(localStorage.getItem("items")?JSON.parse(localStorage.getItem("items")):localStorage.setItem("items","[]"));
 
+api.on("error",(a)=>{
+    console.log(a)
+})
+
 document.addEventListener("DOMContentLoaded",function(){
-    // get tags info
-    // const formulario  = new form("form#Data");
-    // const changeID = formulario.getInput(".Id");
-    // changeID.change(e=>{
-    //     let [Id,Title,Description,date,Done] = e.inputs;
-    //     let [item] = api.getItem(Id.value);
-    //     if(item){
-    //         Title.value = item.title;
-    //         Description.value = item.description;
-    //         date.value = item.date;
-    //         Done.tag.checked = item.status;
-    //     }else{
-    //         Title.value = "";
-    //         Description.value = "";
-    //         date.value = new Date().toISOString().slice(0,10);
-    //         Done.tag.checked = false;
-    //     }
-    // })
-    // formulario.getInput(".Date").value = new Date().toISOString().slice(0,10);
-    // api.on("update",({target:db})=>{
-    //     localStorage.setItem("items",JSON.stringify(db.items))
-        // let id = formulario.getInput(".Id");
-        // id.value = api.items.length;
-        // id.tag.max = id.value; 
-        // document.querySelector("div#print").innerHTML = "";
-        // const modelP = new template("template.modelPrint");
-        // db.items.map(e=>{
-        //     const modelI = new template("template.modelItem");
-        //     // modelI.getChild("button#itemDelete").addEventListener("click",({target})=>
-        //     //     api.delItem(target.parentNode.querySelector(".Id").innerText))
-        //     modelI.insertText("span.Title",e.title);
-        //     modelI.insertText("span.Description",e.description);
-        //     modelI.insertText("span.Date",e.date);
-        //     modelI.insertText("span.Id",e.id);
-        //     modelP.addChild(modelI.tag)
-        // })
-        // modelP.insertIn("#print")
-    // })
-    // api.Update();
-    // formulario.submit(e=>{
-        // let [Id,Title,Description,Date,Done] = e.inputs;
-        // if(api.getItem(Id.value).length>=1){
-        //     let [id_db] = api.getItem(Id.value);
-        //     api.updateItem(id_db.id,{
-        //         title:Title.value,
-        //         description:Description.value,
-        //         date:Date.value,
-        //         status:Done.tag.checked
-        //     });
-        // }else{
-        //     Id.value = api.items.length;
-        //     api.addItem({
-        //         id:Id.value,
-        //         title:Title.value,
-        //         description:Description.value,
-        //         date:Date.value,
-        //         status:Done.tag.checked
-        //     })
-        // }
-    // })
+    const [id,title,description,date,done]  = new form("form#Data").submit(({api:f})=>{
+        const [id,title,description,date,done] = f.getInputs();
+        if(api.getItem(id.value).length>=1){
+            api.updateItem(id.value,{
+                title:title.value,
+                description:description.value,
+                date:date.value,
+                status:done.value
+            });
+        }else{
+            id.value = api.items.length;
+            api.addItem({
+                title:title.value,
+                description:description.value,
+                date:date.value,
+                status:done.value
+            })
+        }
+        title.value = "";
+        description.value = "";
+        date.value = new Date().toISOString().slice(0,10);
+        done.value = false;
+    },true).getInputs();
+    id.change(_=>{
+        let [item] = api.getItem(+id.value);
+        if(item){
+            title.value = item.title;
+            description.value = item.description;
+            date.value = item.date;
+            done.value = item.status;
+        }else{
+            title.value = "";
+            description.value = "";
+            date.value = new Date().toISOString().slice(0,10);
+            done.value = false;
+        }
+    })
+    api.on("update",({target:db})=>{
+        // save in storage
+        localStorage.setItem("items",JSON.stringify(db.items))
+        // clear content
+        document.querySelector("div#print").innerHTML = "";
+        // default content inputs
+        id.default(api.items.length);
+        id.tag.max = api.items.length;
+        date.default(new Date().toISOString().slice(0,10));
+        // system print
+        const modelP = new template("template.modelPrint");
+        db.items.map((e,i)=>{
+            const modelI = new template("template.modelItem");
+            modelI.getChild("button#itemDelete").addEventListener("click",({target})=>
+                api.delItem(target.parentNode.querySelector(".Id").innerText))
+            modelI.insertText("span.Title",e.title);
+            modelI.insertText("span.Description",e.description);
+            modelI.insertText("span.Date",e.date);
+            modelI.insertText("span.Id",i);
+            modelP.insertElement(modelI.tag)
+        })
+        modelP.insertIn("#print")
+    })
+    api.Update();
 })
